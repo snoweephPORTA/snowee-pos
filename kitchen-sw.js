292a4c45bd2ade01527f5eb39d79416078ec7a3e
snoweephPORTA <snoweeph@gmail.com>
2026-05-07 11:43:58 +0800

    Add files via upload

---
+++ kitchen-sw.js
@@ -0,0 +1,53 @@
+const CACHE = 'snowee-kitchen-v1';
+const KITCHEN_URL = '/snowee-pos/kitchen.html';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll([
+        KITCHEN_URL,
+        '/snowee-pos/kitchen-manifest.json',
+        '/snowee-pos/kitchen-icon-192.png',
+        '/snowee-pos/kitchen-icon-512.png',
+      ]))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(KITCHEN_URL))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => {
+        if (cached) return cached;
+        return fetch(e.request)
+          .then(res => {
+            const clone = res.clone();
+            caches.open(CACHE).then(cache => cache.put(e.request, clone));
+            return res;
+          })
+          .catch(() => caches.match(KITCHEN_URL));
+      })
+  );
+});
